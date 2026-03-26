0474ea75cc4da34839afdfb9d15c2dc57f4e4b78
LeScooot <jeronimus.ryan@gmail.com>
2026-03-26 15:02:54 -0400

    launch file

--- hub_ws/src/aruco_detector/launch/aruco_detector.launch.py
+++ hub_ws/src/aruco_detector/launch/aruco_detector.launch.py
@@ -3,28 +3,26 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
-def generate_launch_description():
-    pkg_share = get_package_share_directory('aruco_detector')
-    rviz_config_file = os.path.join(pkg_share, 'config', 'display.rviz')
-    usb_cam_config_file = os.path.join(pkg_share, 'config', 'usb_cam_params.yaml')
 
-    return LaunchDescription([
-        Node(
-            package='usb_cam',
-            executable='usb_cam_node_exe',
-            parameters=[usb_cam_config_file],
-            output='screen'
-        ),
-        Node(
-            package='aruco_detector',
-            executable='aruco_detector',
-            output='screen'
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen',
-            arguments=['-d', rviz_config_file]
-        )
-    ])
+def generate_launch_description():
+    pkg_share = get_package_share_directory("aruco_detector")
+    rviz_config_file = os.path.join(pkg_share, "config", "display.rviz")
+    usb_cam_config_file = os.path.join(pkg_share, "config", "usb_cam_params.yaml")
+
+    return LaunchDescription(
+        [
+            Node( # n cameras - will need to edit this
+                package="usb_cam",
+                executable="usb_cam_node_exe",
+                parameters=[usb_cam_config_file],
+                namespace="camera_front",
+                output="screen",
+            ),
+            Node(  # make n of these for n cameras
+                package="aruco_detector",
+                executable="aruco_detector",
+                output="screen",
+                parameters=[{"camera_name": "camera_front"}],
+            ),
+        ]
+    )
