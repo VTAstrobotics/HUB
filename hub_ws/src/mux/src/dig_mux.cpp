d8592a53798f9ce82e32b97c32c6b9218b64a397
Swara Shinde <swara23@vt.edu>
2026-04-04 19:52:47 -0400

    edited dump mux.

--- hub_ws/src/mux/src/dig_mux.cpp
+++ hub_ws/src/mux/src/dig_mux.cpp
@@ -9,67 +9,96 @@ enum Control_Signal
     TELEOP,
     AUTO
 };
-
+//updated dump mux.
 using std::placeholders::_1;
-class DigMux : public rclcpp::Node
+
+class DumpMux : public rclcpp::Node
 {
 public:
-    DigMux() : Node("dig_mux")
+    DumpMux() : Node("dump_mux")
     {
-        autonomy_subscriber = this->create_subscription<motor_messages::msg::Command>(
-            "/dig_motor_teleop", 10, std::bind(&DigMux::dig_autonomy_callback, this, _1));
+        actuator_autonomy_subscriber = this->create_subscription<motor_messages::msg::Command>(
+            "/dump_actuator_motor_auto", 10, std::bind(&DumpMux::dump_actuator_autonomy_callback, this, _1));
 
-        teleop_subscriber = this->create_subscription<motor_messages::msg::Command>(
-            "/dig_motor_teleop", 10, std::bind(&DigMux::dig_teleop_callback, this, _1));
+        actuator_teleop_subscriber = this->create_subscription<motor_messages::msg::Command>(
+            "/dump_actuator_motor_teleop", 10, std::bind(&DumpMux::dump_actuator_teleop_callback, this, _1));
+
+        door_autonomy_subscriber = this->create_subscription<motor_messages::msg::Command>(
+            "/dump_door_motor_auto", 10, std::bind(&DumpMux::dump_door_autonomy_callback, this, _1));
+
+        door_teleop_subscriber = this->create_subscription<motor_messages::msg::Command>(
+            "/dump_door_motor_teleop", 10, std::bind(&DumpMux::dump_door_teleop_callback, this, _1));
 
         control_signal = this->create_subscription<std_msgs::msg::Int32>(
-            "/dig_control_signal", 10, std::bind(&DigMux::dig_control_signal_callback, this, _1));
+            "/dump_control_signal", 10, std::bind(&DumpMux::dump_control_signal_callback, this, _1));
 
-        dig_motor = std::make_shared<Motor>("dig_motor", this);
+        dump_actuator_motor = std::make_shared<Motor>("dump_actuator_motor", this);
+        dump_door_motor     = std::make_shared<Motor>("dump_door_motor", this);
 
         control_state = TELEOP;
     }
 
 private:
-    // need to create custom messages for autonomy.
-    rclcpp::Subscription<motor_messages::msg::Command>::SharedPtr autonomy_subscriber;
-    rclcpp::Subscription<motor_messages::msg::Command>::SharedPtr teleop_subscriber;
+    // Actuator subscribers
+    rclcpp::Subscription<motor_messages::msg::Command>::SharedPtr actuator_autonomy_subscriber;
+    rclcpp::Subscription<motor_messages::msg::Command>::SharedPtr actuator_teleop_subscriber;
+
+    // Door subscribers
+    rclcpp::Subscription<motor_messages::msg::Command>::SharedPtr door_autonomy_subscriber;
+    rclcpp::Subscription<motor_messages::msg::Command>::SharedPtr door_teleop_subscriber;
+
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr control_signal;
-    std::shared_ptr<Motor> dig_motor;
+
+    std::shared_ptr<Motor> dump_actuator_motor;
+    std::shared_ptr<Motor> dump_door_motor;
 
     Control_Signal control_state;
+    // TODO: define dump action client
 
-    // TODO: define dig action client
-
-    void dig_control_signal_callback(std_msgs::msg::Int32::SharedPtr msg)
+    void dump_control_signal_callback(std_msgs::msg::Int32::SharedPtr msg)
     {
-        control_state = static_cast<Control_Signal>(msg->data);
         if (msg->data < TELEOP || msg->data > AUTO)
             return;
+        control_state = static_cast<Control_Signal>(msg->data);
     }
 
-    void dig_autonomy_callback(motor_messages::msg::Command::SharedPtr msg)
+    void dump_actuator_autonomy_callback(motor_messages::msg::Command::SharedPtr msg)
     {
         if (control_state != AUTO)
             return;
-        dig_motor->send_command(*msg); // handle multiplexing the command type in motor_control
-        RCLCPP_INFO(this->get_logger(), "AUTO COMMAND: DIG");
+        dump_actuator_motor->send_command(*msg);
+        RCLCPP_INFO(this->get_logger(), "AUTO COMMAND: DUMP ACTUATOR");
     }
 
-    void dig_teleop_callback(motor_messages::msg::Command::SharedPtr msg)
+    void dump_actuator_teleop_callback(motor_messages::msg::Command::SharedPtr msg)
     {
         if (control_state != TELEOP)
             return;
+        dump_actuator_motor->send_command(*msg);
+        RCLCPP_INFO(this->get_logger(), "TELEOP COMMAND: DUMP ACTUATOR");
+    }
 
-        dig_motor->send_command(*msg);
-        RCLCPP_INFO(this->get_logger(), "TELEOP COMMAND: DIG");
+    void dump_door_autonomy_callback(motor_messages::msg::Command::SharedPtr msg)
+    {
+        if (control_state != AUTO)
+            return;
+        dump_door_motor->send_command(*msg);
+        RCLCPP_INFO(this->get_logger(), "AUTO COMMAND: DUMP DOOR");
+    }
+
+    void dump_door_teleop_callback(motor_messages::msg::Command::SharedPtr msg)
+    {
+        if (control_state != TELEOP)
+            return;
+        dump_door_motor->send_command(*msg);
+        RCLCPP_INFO(this->get_logger(), "TELEOP COMMAND: DUMP DOOR");
     }
 };
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<DigMux>());
+    rclcpp::spin(std::make_shared<DumpMux>());
     rclcpp::shutdown();
     return 0;
 }
