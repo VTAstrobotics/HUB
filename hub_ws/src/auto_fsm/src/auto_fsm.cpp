c66912dca939e251743dbb4e61c3be42c517ca54
LeScooot <jeronimus.ryan@gmail.com>
2026-04-09 16:57:49 -0400

    adding cases

--- hub_ws/src/auto_fsm/src/auto_fsm.cpp
+++ hub_ws/src/auto_fsm/src/auto_fsm.cpp
@@ -1,5 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
-#include ""
+
+enum AUTO_STATES
+{
+    STARTUP,
+    HOME,
+    DRIVE_TO_DIG,
+    DIG,
+    DRIVE_TO_DUMP,
+    DUMP
+};
 
 class AutoFSM : public rclcpp::Node // You will modify the name
 {
@@ -9,14 +18,34 @@ public:
 
         timer_ptr_ = this->create_wall_timer(0.02s, std::bind(&AutoFSM::fsm_callback, this),
                                              timer_cb_group_);
+        auto_state = STARTUP
     }
 
 private:
     rclcpp::TimerBase::SharedPtr timer_ptr_;
+    AUTO_STATES auto_state;
 
     void fsm_callback()
     {
-	    // fsm
+        switch (auto_state)
+        {
+        case (STARTUP):
+            // button press
+            break;
+        case (HOME):
+            // send home goal
+            break;
+        case (DRIVE_TO_DIG):
+            break;
+        case (DIG):
+            break;
+        case (DRIVE_TO_DUMP):
+            break;
+        case (DUMP):
+            break;
+            default() : break;
+        }
+        // fsm
     }
 };
 
