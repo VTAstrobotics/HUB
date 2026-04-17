4f3b9c5db67b6220ca7df3f55da68033900c0f0d
LeScooot <jeronimus.ryan@gmail.com>
2026-04-17 17:45:38 -0400

    adding teleop launch file

---
+++ hub_ws/src/robot_launch/launch/robot.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
+from launch.conditions import IfCondition, UnlessCondition
+
+
+def generate_launch_description():
+    # Locate the other packages' share directories
+    distributor_share = get_package_share_directory("distributor")
+    dig_share = get_package_share_directory("dig")
+    drive_share = get_package_share_directory("drive")
+    dump_share = get_package_share_directory("dump")
+
+    dig_launch = os.path.join(dig_share, "launch", "dig.launch.py")
+    drive_launch = os.path.join(drive_share, "launch", "old_drive.launch.py")
+    dump_launch = os.path.join(dump_share, "launch", "dump.launch.py")
+
+    spawn_distributor_node = Node(
+        package="distributor",
+        executable="distributor_node",
+        name="distributor_node",
+    )
+
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(PythonLaunchDescriptionSource(dig_launch)),
+            IncludeLaunchDescription(PythonLaunchDescriptionSource(drive_launch)),
+            IncludeLaunchDescription(PythonLaunchDescriptionSource(dump_launch)),
+            spawn_distributor_node,
+        ]
+    )
