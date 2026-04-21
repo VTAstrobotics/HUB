f9d9d839610b9562e56e53458f983d47efa29db7
LeScooot <jeronimus.ryan@gmail.com>
2026-04-21 17:16:50 -0400

    small changes

--- hub_ws/src/dig/src/dig_action_server.cpp
+++ hub_ws/src/dig/src/dig_action_server.cpp
@@ -3,158 +3,168 @@
 #include "rclcpp_components/register_node_macro.hpp"
 
 #include "dig/action/motor_control.hpp"
-
 #include "motor_messages/msg/command.hpp"
 #include "motor_messages/msg/feedback.hpp"
 
 #include <thread>
+#include <memory>
+#include <vector>
 #include <cmath>
 
-// Placeholder, not done yet
-#define DIG_DEPOSIT_POSITION 0
-#define DIG_COLLECT_POSITION 0
-#define DIG_STOW_POSITION 0
-#define DIG_THRESHOLD 1
+using std::placeholders::_1;
+using std::placeholders::_2;
 
-#define DUTY_CYCLE 0.1
-
-using namespace std::placeholders;
 using MotorControl = dig::action::MotorControl;
 using GoalHandleMotor = rclcpp_action::ServerGoalHandle<MotorControl>;
 
+#define DIG_DEPOSIT_POSITION 0.0f
+#define DIG_COLLECT_POSITION 0.0f
+#define DIG_STOW_POSITION 0.0f
+#define DIG_THRESHOLD 1.0f
+
 enum DigPosition
 {
-    COLLECT,
-    DEPOSIT,
-    STOW
+    COLLECT = 0,
+    DEPOSIT = 1,
+    STOW = 2
 };
 
-class dig_action_server : public rclcpp::Node
+class DigActionServer : public rclcpp::Node
 {
 public:
-
-    dig_action_server (const rclcpp::NodeOptions & options)
-        : Node("dig_action_server")
+    explicit DigActionServer(const rclcpp::NodeOptions &options)
+        : Node("dig_action_server", options), dig_position_(0.0f)
     {
-        dig_action_server = rclcpp_action::create_server<MotorControl>(
+        action_server_ = rclcpp_action::create_server<MotorControl>(
             this,
             "motor_control",
-            std::bind(&dig_action_server::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
-            std::bind(&dig_action_server::handle_cancel, this, std::placeholders::_1),
-            std::bind(&dig_action_server::handle_accepted, this, std::placeholders::_1)
+            std::bind(&DigActionServer::handle_goal, this, _1, _2),
+            std::bind(&DigActionServer::handle_cancel, this, _1),
+            std::bind(&DigActionServer::handle_accepted, this, _1));
 
-            door_feedback_subscriber = this->create_subscription<motor_messages::msg::Feedback>("/dig/status", 10, std::bind(&dig_action_server::dig_status_callback, this, _1));
+        dig_feedback_subscriber_ =
+            this->create_subscription<motor_messages::msg::Feedback>(
+                "/dig/status",
+                10,
+                std::bind(&DigActionServer::dig_status_callback, this, _1));
 
-            dig_publisher = this->create_publisher<motor_messages::msg::Command>("/dig/control", 4);
-            
-        );
+        dig_publisher_ =
+            this->create_publisher<motor_messages::msg::Command>("/dig/control", 10);
     }
 
 private:
+    rclcpp_action::Server<MotorControl>::SharedPtr action_server_;
+    rclcpp::Subscription<motor_messages::msg::Feedback>::SharedPtr dig_feedback_subscriber_;
+    rclcpp::Publisher<motor_messages::msg::Command>::SharedPtr dig_publisher_;
 
-    rclcpp_action::Server<MotorControl>::SharedPtr dig_action_server;
-
-    rclcpp::Subscription<motor_messages::msg::Feedback>::SharedPtr dig_feedback_subscriber;
-    rclcpp::Publisher<motor_messages::msg::Command>::SharedPtr dig_publisher;
-
-    float dig_position = 0;
-    float target_position = 0;
+    float dig_position_;
 
     rclcpp_action::GoalResponse handle_goal(
-        const rclcpp_action::GoalUUID & uuid,
+        const rclcpp_action::GoalUUID &uuid,
         std::shared_ptr<const MotorControl::Goal> goal)
     {
-        RCLCPP_INFO(this->get_logger(), "Received goal request with goal %d", goal->target_position);
-
         (void)uuid;
+
+        RCLCPP_INFO(
+            this->get_logger(),
+            "Received goal request: target_position=%d",
+            goal->target_position);
+
+        if (goal->target_position < COLLECT || goal->target_position > STOW)
+        {
+            RCLCPP_WARN(this->get_logger(), "Rejecting invalid goal");
+            return rclcpp_action::GoalResponse::REJECT;
+        }
 
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
-    // Handle cancel request
     rclcpp_action::CancelResponse handle_cancel(
         const std::shared_ptr<GoalHandleMotor> goal_handle)
     {
-        RCLCPP_INFO(this->get_logger(), "Received cancel request");
-
         (void)goal_handle;
+        RCLCPP_INFO(this->get_logger(), "Received cancel request");
         return rclcpp_action::CancelResponse::ACCEPT;
     }
 
     void handle_accepted(const std::shared_ptr<GoalHandleMotor> goal_handle)
     {
-        using namespace std::placeholders;
-        std::thread{std::bind(&dig_action_server::execute, this, std::placeholders::_1), goal_handle}.detach();
+        std::thread{std::bind(&DigActionServer::execute, this, _1), goal_handle}.detach();
     }
 
     void execute(const std::shared_ptr<GoalHandleMotor> goal_handle)
     {
         RCLCPP_INFO(this->get_logger(), "Executing goal");
 
-        DigPosition commandedPosition = static_cast<DigPosition>(goal_handle->get_goal()->target_position);
-        auto feedback = std::make_shared<MotorControl::Feedback>();
-        auto result = std::make_shared<MotorControl::Result>();
+        const auto goal = goal_handle->get_goal();
+        const DigPosition commanded_position =
+            static_cast<DigPosition>(goal->target_position);
 
-        auto & positions = feedback->positions;
-        positions.push_back(dig_position);
+        auto result = std::make_shared<MotorControl::Result>();
+        result->final_positions.push_back(dig_position_);
+
+        auto feedback = std::make_shared<MotorControl::Feedback>();
+        feedback->positions.push_back(dig_position_);
+
+        float target = 0.0f;
+        switch (commanded_position)
+        {
+        case STOW:
+            target = DIG_STOW_POSITION;
+            break;
+        case COLLECT:
+            target = DIG_COLLECT_POSITION;
+            break;
+        case DEPOSIT:
+            target = DIG_DEPOSIT_POSITION;
+            break;
+        default:
+            result->final_positions.push_back(dig_position_);
+            goal_handle->abort(result);
+            RCLCPP_ERROR(this->get_logger(), "Invalid commanded position");
+            return;
+        }
+
+        motor_messages::msg::Command dig_msg;
+        dig_msg.position.data = target;
+        dig_publisher_->publish(dig_msg);
+
         rclcpp::Rate loop_rate(10);
 
-        while (true)
+        while (rclcpp::ok())
         {
-            // Cancel check
             if (goal_handle->is_canceling())
             {
-                result->final_positions = positions;
+                result->final_positions.push_back(dig_position_);
                 goal_handle->canceled(result);
                 RCLCPP_INFO(this->get_logger(), "Goal canceled");
                 return;
             }
 
-            positions.clear();
-            positions.push_back(dig_position);
+            feedback->positions.clear();
+            feedback->positions.push_back(dig_position_);
             goal_handle->publish_feedback(feedback);
-            RCLCPP_INFO(this->get_logger(), "Publish feedback");
 
-            motor_messages::msg::Command dig_msg;
-            bool dig_complete = false;
-
-            switch (commandedPosition)
+            if (std::fabs(dig_position_ - target) <= DIG_THRESHOLD)
             {
-                case STOW:
-
-                    dig_msg.position.data = DIG_STOW_POSITION;
-                    //dig_complete = true;
-                    break;
-                case COLLECT:
-                    dig_msg.position.data = DIG_COLLECT_POSITION;
-                    //dig_complete = true;
-                    break;
-                case DEPOSIT:
-                    dig_msg.position.data = DIG_DEPOSIT_POSITION;
-                    //dig_complete = true;
-                    break;
-                
+                result->final_positions.push_back(dig_position_);
+                goal_handle->succeed(result);
+                RCLCPP_INFO(this->get_logger(), "Goal succeeded");
+                return;
             }
 
-            dig_publisher->publish(dig_msg);
-            //if (dig_complete) break;
             loop_rate.sleep();
-            
         }
 
-        // Final result
-        if (rclcpp::ok()) 
-        {
-            result->final_positions = positions;
-            goal_handle->succeed(result);
-            RCLCPP_INFO(this->get_logger(), "Goal succeeded");
-        }
+        result->final_positions.push_back(dig_position_);
+        goal_handle->abort(result);
+        RCLCPP_ERROR(this->get_logger(), "ROS shutdown before goal completed");
     }
 
-    void dig_status_callback(motor_messages::msg::Feedback msg)
+    void dig_status_callback(const motor_messages::msg::Feedback::SharedPtr msg)
     {
-        dig_position = msg.position.data;
+        dig_position_ = msg->position.data;
     }
 };
 
-RCLCPP_COMPONENTS_REGISTER_NODE(dig_action_server)
+RCLCPP_COMPONENTS_REGISTER_NODE(DigActionServer)
