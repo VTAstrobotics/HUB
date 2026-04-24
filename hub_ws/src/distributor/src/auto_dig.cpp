32271c24432cd2ea29814736d4dbec4a2d3e2f23
LeScooot <jeronimus.ryan@gmail.com>
2026-04-23 21:14:52 -0400

    proper.

--- hub_ws/src/distributor/src/auto_dig.cpp
+++ hub_ws/src/distributor/src/auto_dig.cpp
@@ -12,15 +12,38 @@
 #include "dig/action/motor_control.hpp"
 #include "auto_dig.hpp"
 
-void AutoDig::AutoDig(rclcpp::Node *owner_node)
+AutoDig::AutoDig(rclcpp::Node *owner_node)
 {
     this->owner_node = owner_node;
     velocity_publisher_ = this->owner_node->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
     dig_client_ = rclcpp_action::create_client<MotorControl>(owner_node, "motor_control");
+
+    running = false;
+    cancel_requested = false;
+    dig_goal_succeeded = false;
+}
+
+AutoDig::~AutoDig()
+{
+    cancel_dig();
+
+    if (driver_thread.joinable())
+    {
+        driver_thread.join();
+    }
 }
 
 void AutoDig::auto_dig(float drive_time_seconds)
 {
+    if (running)
+    {
+        RCLCPP_INFO(owner_node->get_logger(), "Auto dig already running");
+        return;
+    }
+    if (driver_thread.joinable())
+    {
+        driver_thread.join();
+    }
     running = true;
 
     driver_thread = std::thread([this, drive_time_seconds]()
@@ -30,12 +53,18 @@ void AutoDig::auto_dig(float drive_time_seconds)
 void AutoDig::cancel_dig()
 {
     cancel_requested = true;
-    geometry_msgs::msg::Twist stop; // all 0
-    velocity_publisher_->publish(stop);
+    geometry_msgs::msg::Twist stop_msg; // all 0
+    velocity_publisher_->publish(stop_msg);
 
-    if (active_goal_handle_)
+    GoalHandleMotor::SharedPtr goal;
     {
-        dig_client_->async_cancel_goal(active_goal_handle_); // cancel current goal
+        std::lock_guard<std::mutex> lock(goal_mutex);
+        goal = active_goal_handle_;
+    }
+
+    if (goal)
+    {
+        dig_client_->async_cancel_goal(goal); // cancel current goal
     }
 }
 
@@ -44,16 +73,48 @@ bool AutoDig::is_running()
     return running;
 }
 
-void AutoDig::run_auto_dig()
+void AutoDig::run_auto_dig(float drive_time_seconds)
 {
-    RCLCPP_INFO(node_->get_logger(), "Starting autodig");
-    send_dig_goal(0);
+    dig_goal_succeeded = false;
+    cancel_requested = false;
+    dig_goal_succeeded = false;
 
+    RCLCPP_INFO(owner_node->get_logger(), "Starting autodig");
+    bool dig_status = send_dig_goal(0);
+    if (!dig_status)
+    {
+        running = false;
+        return;
+    }
+
+    auto start = std::chrono::steady_clock::now();
+    auto timeout = std::chrono::seconds(5);
+
+    while (!dig_goal_succeeded)
+    {
+        if (cancel_requested)
+        {
+            running = false;
+            return;
+        }
+
+        if (std::chrono::steady_clock::now() - start > timeout)
+        {
+            RCLCPP_WARN(owner_node->get_logger(), "Dig goal timeout");
+            dig_goal_succeeded = false;
+            running = false;
+            return;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
+
+    dig_goal_succeeded = false;
     geometry_msgs::msg::Twist cmd;
     cmd.linear.x = 0.2;
     velocity_publisher_->publish(cmd);
 
-    auto start = std::chrono::steady_clock::now();
+    start = std::chrono::steady_clock::now();
 
     while (!cancel_requested)
     {
@@ -73,56 +134,89 @@ void AutoDig::run_auto_dig()
 
     geometry_msgs::msg::Twist stop;
     velocity_publisher_->publish(stop);
-
-    send_dig_goal(1);
-
-    if (cancel_requested_)
+    dig_goal_succeeded = false;
+    dig_status = send_dig_goal(1);
+    if (!dig_status)
     {
         running = false;
-        RCLCPP_WARN(node_->get_logger(), "Autodig canceled during drive");
+        return;
+    }
+    start = std::chrono::steady_clock::now();
+    while (!dig_goal_succeeded)
+    {
+        if (cancel_requested)
+        {
+            running = false;
+            return;
+        }
+
+        if (std::chrono::steady_clock::now() - start > timeout)
+        {
+            RCLCPP_WARN(owner_node->get_logger(), "Dig goal timeout");
+            dig_goal_succeeded = false;
+            running = false;
+            return;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
+
+    if (cancel_requested)
+    {
+        running = false;
+        RCLCPP_WARN(owner_node->get_logger(), "Autodig canceled during drive");
         return;
     }
     running = false;
 }
 
-void AutoDig::send_dig_goal(int target_position)
+bool AutoDig::send_dig_goal(int target_position)
+{
+    if (!dig_client_->wait_for_action_server(std::chrono::seconds(1)))
     {
-        if (!dig_client_->wait_for_action_server(std::chrono::seconds(1)))
+        RCLCPP_ERROR(owner_node->get_logger(), "Dig action server unavailable");
+        return false;
+    }
+    MotorControl::Goal goal;
+    goal.target_position = target_position;
+    rclcpp_action::Client<MotorControl>::SendGoalOptions options;
+
+    options.goal_response_callback =
+        [this](GoalHandleMotor::SharedPtr goal_handle)
+    {
+        if (!goal_handle)
         {
-            RCLCPP_ERROR(node_->get_logger(), "Dig action server unavailable");
+            RCLCPP_ERROR(owner_node->get_logger(), "Dig goal rejected");
             return;
         }
-        MotorControl::Goal goal;
-        goal.target_position = target_position;
-        rclcpp_action::Client<MotorControl>::SendGoalOptions options;
 
-        options.goal_response_callback =
-            [this](GoalHandleMotor::SharedPtr goal_handle)
-            {
-                if (!goal_handle)
-                {
-                    RCLCPP_ERROR(node_->get_logger(), "Dig goal rejected");
-                    return;
-                }
+        {
+            std::lock_guard<std::mutex> lock(goal_mutex);
+            active_goal_handle_ = goal_handle;
+        }
 
-                active_goal_handle_ = goal_handle;
-                RCLCPP_INFO(node_->get_logger(), "Dig goal accepted");
-            };
+        RCLCPP_INFO(owner_node->get_logger(), "Dig goal accepted");
+    };
 
-        options.result_callback =
-            [this](const GoalHandleMotor::WrappedResult & result)
-            {
-                active_goal_handle_.reset();
+    options.result_callback =
+        [this](const GoalHandleMotor::WrappedResult &result)
+    {
+        {
+            std::lock_guard<std::mutex> lock(goal_mutex);
+            active_goal_handle_.reset();
+        }
 
-                if (result.code == rclcpp_action::ResultCode::SUCCEEDED)
-                {
-                    RCLCPP_INFO(node_->get_logger(), "Dig goal succeeded");
-                }
-                else
-                {
-                    RCLCPP_WARN(node_->get_logger(), "Dig goal did not succeed");
-                }
-            };
+        if (result.code == rclcpp_action::ResultCode::SUCCEEDED)
+        {
+            RCLCPP_INFO(owner_node->get_logger(), "Dig goal succeeded");
+            dig_goal_succeeded = true;
+        }
+        else
+        {
+            RCLCPP_WARN(owner_node->get_logger(), "Dig goal did not succeed");
+        }
+    };
 
-        dig_client_->async_send_goal(goal, options);
-    }
+    dig_client_->async_send_goal(goal, options);
+    return true;
+}
--- hub_ws/src/distributor/src/auto_dig.hpp
+++ hub_ws/src/distributor/src/auto_dig.hpp
@@ -15,18 +15,23 @@ class AutoDig
 {
 public:
     AutoDig(rclcpp::Node* owner_node);
+    ~AutoDig()
     void auto_dig(float drive_time_seconds);
     void cancel_dig();
     bool is_running();
 
 private:
     void run_auto_dig(float drive_time_seconds);
-    void send_dig_goal(int target_position);
+    bool send_dig_goal(int target_position);
 
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr velocity_publisher_;
     rclcpp_action::Client<MotorControl>::SharedPtr dig_client_;
     GoalHandleMotor::SharedPtr active_goal_handle_;
     std::thread driver_thread;
     rclcpp::Node* owner_node; 
-    bool running
+    std::atomic_bool running;
+    std::atomic_bool cancel_requested;
+    std::atomic_bool dig_goal_succeeded;
+
+    std::mutex goal_mutex;
 };
