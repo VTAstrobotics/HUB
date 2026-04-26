cb9175e64174a2a4465a25df98591e19d147f8a5
Joaquin Fernandez <joaquinfernandez7002@gmail.com>
2026-04-26 21:12:24 +0000

    Parameter for follower motors

--- hub_ws/src/motor_control/src/kraken_controller.cpp
+++ hub_ws/src/motor_control/src/kraken_controller.cpp
@@ -10,8 +10,11 @@ void KrakenController::control_callback(const motor_messages::msg::Command::Shar
 {
 
   constexpr float EPS = 1e-6f;
-
-  if (abs(msg->dutycycle.data) > EPS)
+  if (follower != nullptr)
+  {
+    this->motor->SetControl(this->follower);
+  }
+  else if (abs(msg->dutycycle.data) > EPS)
   {
     this->outDuty.Output = msg->dutycycle.data;
 
