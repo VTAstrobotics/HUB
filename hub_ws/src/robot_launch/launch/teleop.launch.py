2e5f3f91fa12869490c6dd33fbfb9758729c0426
LeScooot <jeronimus.ryan@gmail.com>
2026-04-30 12:09:42 -0400

    nav2 setup

--- hub_ws/src/robot_launch/launch/teleop.launch.py
+++ hub_ws/src/robot_launch/launch/teleop.launch.py
@@ -24,6 +24,8 @@ def generate_launch_description():
     drive_launch = os.path.join(drive_share, "launch", "old_drive.launch.py")
     dump_launch = os.path.join(dump_share, "launch", "dump.launch.py")
     reaper_description_launch = os.path.join(reaper_description_share, "launch", "launch.py")
+
+
     ukf_launch_launch = reaper_description_launch = os.path.join(ukf_launch_share, "launch", "ukf.launch.py")
 
 
@@ -62,17 +64,46 @@ def generate_launch_description():
         'h264.tune':    'zerolatency',
     }]
 )        
+    
+
+    base_link_to_camera_link = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_to_camera_link_publisher',
+        arguments=[
+            '--x', '0.2', 
+            '--y', '0.0', 
+            '--z', '0.3', 
+            '--roll', '0.0', 
+            '--pitch', '0.0', 
+            '--yaw', '0.0', 
+            '--frame-id', 'base_link', 
+            '--child-frame-id', 'zed_camera_link'
+        ]
+    )
+
+    nav2_bringup_share = get_package_share_directory('nav2_bringup')
+    our_nav_share = get_package_share_directory('navigation')
+    nav2_params = os.path.join(our_nav_share, 'config', 'nav2_params.yaml')
+    nav2_launch = os.path.join(nav2_bringup_share, 'launch', 'navigation_launch.py')
 
 
     nodes = [
             IncludeLaunchDescription(PythonLaunchDescriptionSource(dig_launch)),
             IncludeLaunchDescription(PythonLaunchDescriptionSource(drive_launch)),
             IncludeLaunchDescription(PythonLaunchDescriptionSource(dump_launch)),
-            IncludeLaunchDescription(PythonLaunchDescriptionSource(reaper_description_launch)),
+            # IncludeLaunchDescription(PythonLaunchDescriptionSource(reaper_description_launch)),
             IncludeLaunchDescription(PythonLaunchDescriptionSource(ukf_launch_launch)),
             spawn_distributor_node,
             foxglove_studio,
-            zed_h264_republisher
+            zed_h264_republisher,
+            base_link_to_camera_link,
+            IncludeLaunchDescription(PythonLaunchDescriptionSource(nav2_launch), 
+                                 launch_arguments={
+                                    'use_sim_time': 'false',
+                                    'params_file': nav2_params,
+                                }.items()
+                            )
         ]
 
     if found_zed:
