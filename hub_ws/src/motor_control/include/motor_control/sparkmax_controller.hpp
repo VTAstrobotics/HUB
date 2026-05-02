2cca804f5d26b029cceeca59937fd218eeef97af
Andrew Walls <awalls03@vt.edu>
2026-05-02 16:59:02 -0400

    Adding PIDF as parameters in the sparkmax_controller class

--- hub_ws/src/motor_control/include/motor_control/sparkmax_controller.hpp
+++ hub_ws/src/motor_control/include/motor_control/sparkmax_controller.hpp
@@ -32,10 +32,19 @@ public:
 
     motor = std::make_unique<SparkMax>(can_interface, static_cast<uint8_t>(can_id));
 
-    motor->SetP(0, 0.00008);
-    motor->SetI(0, 0.0);
-    motor->SetD(0, 0.0001);
-    motor->SetF(0, 0.00017);
+    this->declare_parameter<float>("kP", 0);
+    this->declare_parameter<float>("kI", 0);
+    this->declare_parameter<float>("kD", 0);
+    this->declare_parameter<float>("kF", 0);
+
+    double kP = this->get_parameter("kP").as_float();
+    motor->SetP(0, kP);
+    double kI = this->get_parameter("kI").as_float();
+    motor->SetI(0, kI);
+    double kD = this->get_parameter("kD").as_float();
+    motor->SetD(0, kD);
+    double kF = this->get_parameter("kF").as_float();
+    motor->SetF(0, kF);
     motor->BurnFlash();
 
     float velocityConversion = motor->GetVelocityConversionFactor();
